408bc5f60b0afffd3291f841babe04242cffdb97
Randy Armstrong <randy@sparhawksoftware.com>
2020-08-04 15:52:39 -0700

    Remove obsolete files.

--- Core/Types/Schemas/BuildSchema.bat
+++
@@ -1,9 +0,0 @@
-﻿@echo off
-setlocal
-
-echo Processing NodeSet Schema
-xsd /classes /n:Opc.Ua.Schema.Binary OPCBinarySchema.xsd
-
-echo #pragma warning disable 1591 > temp.txt
-type OPCBinarySchema.cs >> temp.txt
-type temp.txt > OPCBinarySchema.cs
