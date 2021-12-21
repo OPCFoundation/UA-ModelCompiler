c9adbbbd9b3da6a60a83d6f6748058206946f365
Martin Regen <mregen@microsoft.com>
2021-12-21 09:17:34 +0100

    add github actions

---
+++ .azurepipelines/get-version.ps1
@@ -0,0 +1,30 @@
+<#
+ .SYNOPSIS
+    Sets CI version build variables and/or returns version information.
+
+ .DESCRIPTION
+    The script is a wrapper around the versioning tool.
+#>
+
+try {
+    # Try install tool
+    & dotnet @("tool", "install", "--tool-path", "./tools", "--version", "3.4.244", "--framework", "netcoreapp3.1", "nbgv") 2>&1 
+
+    $props = (& ./tools/nbgv  @("get-version", "-f", "json")) | ConvertFrom-Json
+    if ($LastExitCode -ne 0) {
+        throw "Error: 'nbgv get-version -f json' failed with $($LastExitCode)."
+    }
+
+    return [pscustomobject] @{ 
+        Full = $props.CloudBuildAllVars.NBGV_Version
+        Pre = $props.CloudBuildAllVars.NBGV_PrereleaseVersion
+        Public = $props.CloudBuildAllVars.NBGV_PublicRelease
+        Nuget = $props.CloudBuildAllVars.NBGV_NuGetPackageVersion
+        Prefix = $props.CloudBuildAllVars.NBGV_SimpleVersion
+        Revision = $props.CloudBuildAllVars.NBGV_VersionRevision
+    }
+}
+catch {
+    Write-Warning $_.Exception
+    return $null
+}
